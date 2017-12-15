ac47d1636dd67f66ea2baa1f9d39cb6d0874822d
stephens999 <stephens999@gmail.com>
2017-12-15 12:58:08 -0600

    update instructions

--- R/ebnm.R
+++ R/ebnm.R
@@ -1,14 +1,22 @@
-#' @title Solve the Empirical Bayes Normal Means problem
-#' @details Given vectors of data x, and standard errors s, fit the point laplace prior
-#' (i.e. a mixture of point mass at 0 and Laplace distribution)
-#' by fitting the two parameters (pi0 and laplace rate). The model and code are based on
+#' @title Solve the Empirical Bayes Normal Means problem with point-laplace prior
+#' @details Given vectors of data x, and standard errors s, solve EBNM problem with "point-laplace" prior
+#' (i.e. a mixture of point mass at 0 and Laplace distribution).
+#' That is the prior is pi0 \delta_0 + (1-pi0)DExp(a) where Dexp is the double exponential
+#' (Laplace) distribution, and (pi0,a) are esimated by marginal maximum likelihood. The model and code are based on
 #' EbayesThresh by Johnstone and Silverman, but simplified by removing thresholding, and
-#' improved numerical stability
+#' improved numerical stability.
 #' @param x a vector of observations
 #' @param s a vector of standard deviations (or scalar if all equal)
 #' @return a list with elements result, fitted_g, and loglik
+#' @examples
+#' mu = c(rep(0,1000), rexp(1000)) # means
+#' s = rgamma(2000,1,1) #standard errors
+#' x = mu + rnorm(2000,0,s) # observations
+#' x.ebnm = ebnm_point_laplace(x,s)
+#' ashr::get_pm(x.ebnm) # posterior mean
+#'
 #' @export
-ebnm <- function (x,s=1) {
+ebnm_point_laplace <- function (x,s=1) {
   #could consider makign more stable this way? But might have to be careful with log-likelihood
   #m_sdev <- mean(s)
   #s <- s/m_sdev
