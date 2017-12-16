f82530fb75448ef9a0ce0c573c25b02e97282ff9
stephens999 <stephens999@gmail.com>
2017-12-15 23:16:41 -0600

    add some gradient computations on logit(w) and log(a) scale

--- R/grad.R
+++ R/grad.R
@@ -13,8 +13,27 @@ grad_negloglik_a = function(x,s,w,a){
   -w * sum(grad_lg(x,s,a)/((1-w)*f_over_g+w))
 }
 
+# combines the two above
 grad_negloglik  = function(x,s,w,a){
-  c(grad_negloglik_w(x,s,w,a),grad_negloglik_a(x,s,w,a))
+  l = vloglik.laplace(x,s,w,a)
+  lf = dnorm(x,0,s,log=TRUE)
+  lg = logg.laplace(x,s,a)
+  grad_w = sum(exp(lf-l)-exp(lg-l))
+
+  f_over_g = exp(lf-lg)
+  grad_a = -w * sum(grad_lg(x,s,a)/((1-w)*f_over_g+w))
+
+  c(grad_w,grad_a)
+}
+
+
+
+# computes gradient with respect to logit(w) and log(a)
+grad_negloglik.logscale  = function(x,s,w,a){
+  grad = grad_negloglik(x,s,w,a)
+  grad[1] = grad[1] * (w*(1-w))
+  grad[2] = grad[2] * a
+  return(grad)
 }
 
 # set.seed(1)
--- R/wandafromx.mle.R
+++ R/wandafromx.mle.R
@@ -18,7 +18,6 @@ wandafromx.mle <- function(x, s,control=NULL) {
 
   uu <- optim(startpar, function(par,x,s){-loglik.laplace(x,s,par[1],par[2])}, method="L-BFGS-B",
                 lower = lo, upper = hi, x = x, s = s, control=control)
-  message(paste(uu$counts))
   uu <- uu$par
 
   return(list(w=uu[1], a=uu[2]))
@@ -41,13 +40,67 @@ wandafromx.mle.grad <- function(x, s, control=NULL) {
   uu <- optim(startpar, function(par,x,s){-loglik.laplace(x,s,par[1],par[2])},
               gr= function(par,x,s){grad_negloglik(x,s,par[1],par[2])}, method="L-BFGS-B",
               lower = lo, upper = hi, x = x, s = s, control=control)
-  message(paste(uu$counts))
   uu <- uu$par
 
   return(list(w=uu[1], a=uu[2]))
 }
 
+#do optimization of parameters on log scale
+wandafromx.mle.logscale <- function(x, s,control=NULL) {
+
+  # get some reasonable limits on sigma (standard deviation of laplace, or 1/sqrt(a))
+  sigmamin = min(s)/10
+  if (all(x^2 <= s^2)) {
+    sigmamax = 8 * sigmamin
+  } else {
+    sigmamax = 2 * sqrt(max(x^2 - s^2))
+  }
+
+  lo  <-  c(-5,log(1/sigmamax^2))
+  hi  <-  c(5,log(1/sigmamin^2))
+  startpar  <- c(0,log(2/(sigmamax^2+sigmamin^2)))
+
+  uu <- optim(startpar,
+              function(par,x,s){-loglik.laplace(x,s,exp(par[1])/(1+exp(par[1])),
+                                                exp(par[2]))},
+              method="L-BFGS-B",
+              lower = lo, upper = hi, x = x, s = s, control=control)
+  uu <- uu$par
+
+  return(list(w=exp(uu[1])/(1+exp(uu[1])), a=exp(uu[2])))
+}
 
 
+#do optimization of parameters on log scale
+wandafromx.mle.logscale.grad <- function(x, s,control=NULL) {
+
+  # get some reasonable limits on sigma (standard deviation of laplace, or 1/sqrt(a))
+  sigmamin = min(s)/10
+  if (all(x^2 <= s^2)) {
+    sigmamax = 8 * sigmamin
+  } else {
+    sigmamax = 2 * sqrt(max(x^2 - s^2))
+  }
+
+  lo  <-  c(-5,log(1/sigmamax^2))
+  hi  <-  c(5,log(1/sigmamin^2))
+  startpar  <- c(0,log(2/(sigmamax^2+sigmamin^2)))
+
+  uu <- optim(startpar,
+              function(par,x,s){-loglik.laplace(x,s,exp(par[1])/(1+exp(par[1])),
+                                                exp(par[2]))},
+              gr= function(par,x,s){grad_negloglik.logscale(x,s,exp(par[1])/(1+exp(par[1])),
+                                                            exp(par[2]))},
+              method="L-BFGS-B",
+              lower = lo, upper = hi, x = x, s = s, control=control)
+  uu <- uu$par
+
+  return(list(w=exp(uu[1])/(1+exp(uu[1])), a=exp(uu[2])))
+}
 
 
+# x = rnorm(10000) + c(rep(0,5000),rexp(5000))
+# s = 1
+# microbenchmark::microbenchmark(wandafromx.mle.grad(x,s),times=10)
+# microbenchmark::microbenchmark(wandafromx.mle(x,s),times=10)
+# microbenchmark::microbenchmark(wandafromx.mle.logscale.grad(x,s),times=10)
