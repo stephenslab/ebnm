b5e0b34084bb41c8d1f52dddff46dd26ce6cefbd
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-01-01 14:39:17 -0600

    Added dependency to ashr >= 2.2-2.

--- tests/testthat/test_normal.R
+++ tests/testthat/test_normal.R
@@ -6,7 +6,9 @@ test_that("postmean gives same result as ashr",{
   ebnm.res = ebnm_point_normal(x,s)
   pi0 = ebnm.res$fitted_g$pi0
   a = ebnm.res$fitted_g$a
-  ash.res = ashr::ash(x,s,outputlevel=5,method="shrink",g=ashr::normalmix(c(pi0,1-pi0),c(0,0),c(0,sqrt(1/a))),fixg=TRUE)
+  ash.res = ashr::ash(x,s,outputlevel = 5,method = "shrink",
+                      g = ashr::normalmix(c(pi0,1-pi0),c(0,0),c(0,sqrt(1/a))),
+                      fixg = TRUE)
   expect_equal(ebnm.res$result$PosteriorMean,ash.res$flash_data$postmean)
   expect_equal(ebnm.res$result$PosteriorMean2,ash.res$flash_data$postmean2)
   expect_equal(ebnm.res$loglik,ash.res$flash_data$penloglik)
