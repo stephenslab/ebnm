fcb607c27b8fc15549fed2321cd96af11c64673c
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-07-16 09:59:45 -0500

    Add option to fix pi0; add wrapper function for fitting normal prior; clean up documentation; refactor functions for maintainability

--- R/ebnm_point_normal.R
+++ R/ebnm_point_normal.R
@@ -1,78 +1,165 @@
-#' @title Solve the Empirical Bayes Normal Means problem with point-normal prior
-#' @description This function solves the Empirical Bayes Normal Means problem with point-normal prior.
+#' @title Solve the EBNM problem with point-normal prior
 #'
-#' @details Given vectors of data x, and standard errors s, solve the EBNM problem with "point-normal" prior
-#' That is, the model is $x_j \sim N(\theta_j,s_j^2)$, with $s_j$ given, and $\theta_j \sim g$
-#' where $g$ is a mixture of point mass at 0 and normal distribution: \eqn{pi0 \delta_0 + (1-pi0)N(0,1/a)} where N is the normal
-#' distribution, and (pi0,a) are estimated by marginal maximum likelihood.
-#' @param x a vector of observations
-#' @param s a vector of standard deviations (or scalar if all equal)
-#' @param g The prior distribution (list with elements pi0,a). Usually this is unspecified (NULL) and
-#' estimated from the data. However, it can be used in conjuction with fixg=TRUE
-#' to specify the g to use (e.g. useful in simulations to do computations with the "true" g).
-#' Or, if g is specified but fixg=FALSE, the g specifies the initial value of g used before optimization.
-#' @param fixg If TRUE, don't estimate g but use the specified g.
-#' @param norm normalization factor to divide x and s by before running optimization (should not affect
-#' results, but improves numerical stability when x and s are tiny).
-#' @param output vector of strings indicating what values to be returned.
-#' Options include: "result" (summary results), "fitted_g" (the fitted prior), "loglik", and
-#' "post_sampler" (a function that can be used to produce posterior samples; takes a single
-#' parameter nsamp, the number of posterior samples to return per observation).
+#' @description This function solves the Empirical Bayes Normal Means
+#'   problem with a point-normal prior.
 #'
-#' @return a list with elements specified by output parameter
+#' @details Given vectors of data \code{x} and standard errors \code{s},
+#'   solve the EBNM problem with a "point-normal" prior. The model is
+#'  \deqn{x_j \sim N(\theta_j, s_j^2),} where \eqn{s_j} are given and
+#'   \eqn{\theta_j \sim g}, with \eqn{g} a mixture of a point mass at
+#'   zero and a normal distribution: \deqn{\theta_j \sim \pi_0 \delta_0
+#'   + (1 - \pi_0)N(0, 1/a).} \eqn{\pi_0} and \eqn{a} are estimated by
+#'   marginal maximum likelihood.
+#'
+#' @param x A vector of observations.
+#'
+#' @param s A vector of standard deviations (or a scalar if all are
+#'   equal).
+#'
+#' @param g The prior distribution (a list with elements \code{pi0} and
+#'   \code{a}). Usually this is unspecified (\code{NULL}) and estimated
+#'   from the data. However, it can be used in conjuction with
+#'   \code{fixg = TRUE} to specify the prior to use (useful, for example,
+#'   to do computations with the "true" \code{g}). Or, if \code{g} is
+#'   specified but \code{fixg = FALSE}, \code{g} specifies the initial
+#'   value of \code{g} used before optimization.
+#'
+#' @param fixg If \code{TRUE}, use the specified \code{g} instead of
+#'   estimating it.
+#'
+#' @param fix_pi0 If \code{TRUE}, \code{g$pi0} is fixed at the supplied
+#'   value and \code{g$a} is estimated from the data. \code{fixg = TRUE}
+#'   overrides \code{fix_pi0 = TRUE}. That is, if both are \code{TRUE}
+#'   then both \code{pi0} and \code{a} are fixed at the supplied values.
+#'
+#' @param norm The normalization factor to divide \code{x} and \code{s}
+#'   by before running optimization (this should not affect results, but
+#'   it improves numerical stability when \code{x} and \code{s} are
+#'   tiny).
+#'
+#' @param output A vector of strings indicating which values are to be
+#' returned. Options include:
+#' \describe{
+#'   \item{"result"}{Summary results (posterior means \eqn{E \theta_j}
+#'     and posterior values of \eqn{E \theta_j^2}).}
+#'   \item{"fitted_g"}{The fitted prior (a list with elements \code{pi0}
+#'     and \code{a}).}
+#'   \item{"loglik"}{The optimal log likelihood attained.}
+#'   \item{"post_sampler"}{A function that can be used to produce
+#'     samples from the posterior. It takes a single parameter
+#'     \code{nsamp}, the number of posterior samples to return per
+#'     observation.}
+#'  }
+#'
+#' @return A list with elements specified by parameter \code{output}.
+#'
 #' @examples
-#' mu = c(rep(0,1000), rexp(1000)) # means
-#' s = rgamma(2000,1,1) #standard errors
-#' x = mu + rnorm(2000,0,s) # observations
-#' x.ebnm = ebnm_point_normal(x,s)
+#' mu = c(rep(0, 1000), rexp(1000)) # means
+#' s = rgamma(2000, 1, 1) #standard errors
+#' x = mu + rnorm(2000, 0, s) # observations
+#' x.ebnm = ebnm_point_normal(x, s)
 #' ashr::get_pm(x.ebnm) # posterior mean
 #'
 #' @export
-ebnm_point_normal <- function (x, s=1, g=NULL, fixg=FALSE, norm=mean(s), output=c("result","fitted_g","loglik")) {
-  output = set_output(output)
+ebnm_point_normal <- function (x,
+                               s = 1,
+                               g = NULL,
+                               fixg = FALSE,
+                               fix_pi0 = FALSE,
+                               norm = mean(s),
+                               output = c("result","fitted_g","loglik"),
+                               control = NULL) {
+  output <- set_output(output)
 
-  if(is.null(g) & fixg){stop("must specify g if fixg=TRUE")}
-  if(!is.null(g) & !fixg){stop("option to intialize from g not yet implemented")}
-
-  if (fixg) {
-    norm = 1 # If g is fixed, don't scale
+  if (fixg && is.null(g)) {
+    stop("Must specify g if fixg = TRUE")
+  }
+  if (fix_pi0 && is.null(g$pi0)) {
+    stop("Must specify g$pi0 if fix_pi0 = TRUE")
+  }
+  if (!is.null(g$a) && (g$a <= 0)) {
+    stop("Invalid choice of g$a")
+  }
+  if (!is.null(g$pi0) && (g$pi0 < 0 || g$pi0 > 0)) {
+    stop("Invalid choice of g$pi0")
   }
 
-  # Scale for stability, but need to be careful with log-likelihood
-  s <- s/norm
-  x <- x/norm
+  # Scale for stability, but need to be careful with log-likelihood:
+  s <- s / norm
+  x <- x / norm
+  if (!is.null(g) && !is.null(g$a)) {
+    g$a <- g$a * norm^2
+  }
 
-  # Estimate g from data
+  # Estimate g from data:
   if (!fixg) {
-    g <- mle_normal_logscale_grad(x, s)
+    if (fix_pi0) {
+      g <- mle_normal_logscale_fixed_pi0(x, s, g, control)
+    } else {
+      g <- mle_normal_logscale_grad(x, s, g, control)
+    }
   }
 
 	w <- 1 - g$pi0
 	a <- g$a
 
+	# Compute return values, taking care to adjust back to original scale:
 	retlist <- list()
 
-	# Compute return values, taking care to adjust results back to original scale
-  if ("result" %in% output) {
-    result <- compute_summary_results_normal(x,s,w,a)
+	if ("result" %in% output) {
+    result <- compute_summary_results_normal(x, s, w, a)
     result$PosteriorMean <- result$PosteriorMean * norm
     result$PosteriorMean2 <- result$PosteriorMean2 * norm^2
     retlist <- c(retlist, list(result=result))
   }
+
 	if ("fitted_g" %in% output) {
-	  fitted_g = list(pi0 = g$pi0, a = g$a/(norm^2))
+	  fitted_g = list(pi0 = g$pi0, a = g$a / norm^2)
 	  retlist <- c(retlist, list(fitted_g=fitted_g))
 	}
+
 	if ("loglik" %in% output) {
-	  loglik <- loglik_normal(x,s,w,a)
-	  loglik <- loglik - length(x)*log(norm)
+	  loglik <- loglik_normal(x, s, w, a)
+	  loglik <- loglik - length(x) * log(norm)
 	  retlist <- c(retlist, list(loglik=loglik))
 	}
+
 	if ("post_sampler" %in% output) {
 	  retlist <- c(retlist, list(post_sampler = function(nsamp) {
-	    norm * post_sampler_normal(x, s, w, a, nsamp)
+	    post_sampler_normal(x, s, w, a, nsamp) * norm
 	  }))
 	}
 
 	return(retlist)
+}
+
+# Wrapper function for N(0, 1/a) prior (no pointmass).
+#
+#' @title Solve the EBNM problem with normal prior
+#'
+#' @description This function solves the Empirical Bayes Normal Means
+#'   problem with a normal prior.
+#'
+#' @inherit ebnm_point_normal
+#'
+#' @details Given vectors of data \code{x} and standard errors \code{s},
+#'   solve the EBNM problem with a normal prior. The model is
+#'   \deqn{x_j \sim N(\theta_j, s_j^2),} where \eqn{s_j} are given
+#'   and \deqn{\theta_j \sim N(0, 1/a).} \eqn{a} is estimated by
+#'   marginal maximum likelihood.
+#'
+#' @export
+#'
+ebnm_normal <- function (x,
+                         s = 1,
+                         norm = mean(s),
+                         output = c("result","fitted_g","loglik"),
+                         control = NULL) {
+  ebnm_point_normal(x = x,
+                    s = s,
+                    g = list(pi0 = 0),
+                    fix_pi0 = TRUE,
+                    norm = norm,
+                    output = output,
+                    control = control)
 }
--- R/loglik_normal.R
+++ R/loglik_normal.R
@@ -1,30 +1,39 @@
-# Functions to compute the log likelihood under the normal prior
-# More numerically stable than the original approach in negloglik_normal.R
+# Functions to compute the log likelihood under the normal prior.
 
-# This is the log of density of normal(0,1/a) convolved with normal(0,s)
+
+# The log density of normal(0, 1/a) convolved with normal(0, s).
 #
 #' @importFrom stats dnorm
-logg_normal = function(x,s,a){
-  dnorm(x,0,sqrt(s^2+1/a),log=TRUE)
+#'
+logg_normal = function(x, s, a) {
+  dnorm(x, 0, sqrt(s^2 + 1/a), log=TRUE)
 }
 
-# return log((1-w)f + wg) as a vector
-# deal with case w=1 and w=0 separately for stability
+
+# Return log((1-w)f + wg) as a vector.
+# Deal with cases w = 1 and w = 0 separately for stability.
 #
 #' @importFrom stats dnorm
-vloglik_normal = function(x,s,w,a){
-  if(w<=0){return(dnorm(x,0,s,log=TRUE))}
-  lg = logg_normal(x,s,a)
-  if(w>=1){return(lg)}
+#'
+vloglik_normal = function(x, s, w, a) {
+  if (w <= 0) {
+    return(dnorm(x, 0, s, log = TRUE))
+  }
+  lg = logg_normal(x, s, a)
+  if (w >= 1) {
+    return(lg)
+  }
 
-  lf = dnorm(x,0,s,log=TRUE)
-  lfac = pmax(lg,lf)
-  result = lfac + log((1-w)*exp(lf-lfac) + w*exp(lg-lfac))
-  result[is.infinite(lf)] = log(1-w) # handles the case where s=0, so we get infinite density
+  lf = dnorm(x, 0, s, log = TRUE)
+  lfac = pmax(lg, lf)
+  result = lfac + log((1 - w) * exp(lf - lfac) + w * exp(lg - lfac))
+  # Handle the case where s = 0, so that the density is infinite:
+  result[is.infinite(lf)] = log(1 - w)
 
   return(result)
 }
 
-loglik_normal = function(x,s,w,a){
-  sum(vloglik_normal(x,s,w,a))
+
+loglik_normal = function(x, s, w, a) {
+  sum(vloglik_normal(x, s, w, a))
 }
