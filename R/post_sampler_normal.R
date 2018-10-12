1c5f45e589fba4f1ef935546badfcb29906a18be
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-12 16:22:02 -0500

    Updated functions for normal case

--- R/post_sampler_normal.R
+++ R/post_sampler_normal.R
@@ -1,10 +1,10 @@
-# @title Sample from the posterior under point-normal prior
+# @title Sample from the posterior under normal prior
 #
-# @inheritParams ebnm_point_normal
+# @inheritParams ebnm_normal
 #
-# @param w The weight of the non-null component in the fitted prior.
+# @param mu The mean of the normal in the fitted prior.
 #
-# @param a The variance of the non-null component in the fitted prior.
+# @param a The precision of normal in the fitted prior.
 #
 # @param nsamp The number of samples to return per observation.
 #
@@ -13,13 +13,11 @@
 #
 #' @importFrom stats rbinom rnorm
 #'
-post_sampler_point_normal <- function(x, s, w, a, nsamp) {
-  wpost <- wpost_normal(x, s, w, a)
-  pmean_cond <- pmean_cond_normal(x, s, a)
-  pvar_cond <- pvar_cond_normal(s, a)
+post_sampler_normal <- function(x, s, mu, a, nsamp) {
+  pmean_cond <- pmean_cond_normal_mu(x, s, mu, a)
+  pvar_cond <- pvar_cond_normal_mu(s, a)
   
-  is_nonnull <- rbinom(nsamp*length(wpost), 1, rep(wpost, nsamp))
-  samp <- is_nonnull * rnorm(nsamp*length(wpost), rep(pmean_cond, nsamp),
+  samp <- rnorm(nsamp*length(pmean_cond), rep(pmean_cond, nsamp),
                              rep(sqrt(pvar_cond), nsamp))
-  return(matrix(samp, nrow=nsamp, byrow=T))
+  return(matrix(samp, nrow = nsamp, byrow = T))
 }
