ba4bcab4e18c061bce9199220f1b77e999be16ed
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-12 15:56:46 -0500

    Updating with implemented functions

--- R/compute_summary_results_normal.R
+++ R/compute_summary_results_normal.R
@@ -1,53 +1,32 @@
-compute_summary_results_point_normal = function(x, s, w, a){
-  wpost <- wpost_normal(x, s, w, a)
-  pmean_cond <- pmean_cond_normal(x, s, a)
-  pvar_cond <- pvar_cond_normal(s, a)
+compute_summary_results_normal = function(x, s, mu, a){
+  pmean_cond <- pmean_cond_normal_mu(x, s, a)
+  pvar_cond <- pvar_cond_normal_mu(s, a)
   
-  PosteriorMean <- wpost * pmean_cond
-  PosteriorMean2 <- wpost * (pmean_cond^2 + pvar_cond)
+  PosteriorMean <- pmean_cond
+  PosteriorMean2 <- (pmean_cond^2 + pvar_cond)
   
   return(data.frame(PosteriorMean = PosteriorMean,
                     PosteriorMean2 = PosteriorMean2))
 }
 
-#
-#  Calculate the posterior weight for non-zero effect
-#
-#' @importFrom stats dnorm
-#'
-wpost_normal <- function(x, s, w, a) {
-  if (w == 0) {
-    return(rep(0, length(x)))
-  }
-  if (w == 1) {
-    return(rep(1, length(x)))
-  }
-  
-  lg <- dnorm(x, 0, sqrt(s^2 + 1/a), log = TRUE)
-  lf <- dnorm(x, 0, s, log = TRUE)
-  wpost <- w / (w + (1 - w) * exp(lf - lg))
-  
-  # Deal with zero and infinite sds:
-  wpost[s == 0 & x == 0] <- 0
-  wpost[s == 0 & x != 0] <- 1
-  wpost[is.infinite(s)] <- w
-  
-  return(wpost)
-}
+
 
 #
-#  Calculate the posterior mean for non-zero effect
+#  Calculate the posterior mean effect
 #
-pmean_cond_normal <- function(x, s, a) {
-  return(x / (1 + s^2 * a))
+pmean_cond_normal_mu <- function(x, s, mu, a) {
+  if (is.infinite(a)) { # if prior precision is infinite, posterior is proir mean
+    return(rep(mu, length(x)))
+  }
+  return((x + (s^2) * a * mu) / (1 + s^2 * a))
 }
 
 #
 #  Calculate the posterior variance for non-zero effect
 #
-pvar_cond_normal <- function(s, a) {
-  pvar_cond <- s^2 / (1 + s^2 * a)
-  pvar_cond[is.infinite(s)] <- 1 / a
+pvar_cond_normal_mu <- function(s, a) {
+  pvar_cond <- rep(1 / a, length(s))
+  pvar_cond[!is.infinite(s)] <- s^2 / (1 + s^2 * a)
   
   return(pvar_cond)
 }
