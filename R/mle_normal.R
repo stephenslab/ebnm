e114f0139ac4c152ab7d1ff07084d3e11c9f8d75
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-11 23:05:11 -0500

    Updated functions. Note that the hilo function is commented out, and the optimization doesn't try a second time

--- R/mle_normal.R
+++ R/mle_normal.R
@@ -1,26 +1,19 @@
-# @title Compute MLE under point-normal prior
+# @title Compute MLE under normal prior with no point-mass and arbitrary mean mu
 #
-# @description Computes MLE of w and a for data (x, s) under a
-#   point-normal prior.
+# @description Computes MLE of mu and a for data (x, s) under a
+#   normal prior.
 #
 # @param x Observations.
 #
 # @param s Standard deviations.
 #
-# @param g List with initial values for g$a and g$pi0.
+# @param g List with initial values for g$a and g$mu.
 #
 # @param control List of parameters to be passed to \code{optim}.
 #
-mle_point_normal_logscale_grad <- function(x, s, g, control) {
+mle_normal_logscale_grad <- function(x, s, g, control) {
   # Do optimization of parameters on log scale (the parameters are
-  #   -logit(pi0) and log(a)).
-  
-  maxvar <- max(x^2 - s^2) # Get upper bound on variance estimate.
-  
-  # Deal with case where everything is smaller than expected (null):
-  if (maxvar < 0) {
-    return(list(pi0 = 1, a = 1)) # Note that a is irrelevant if pi0 = 1.
-  }
+  #   mu and log(a)).
   
   # Set default starting point. This point is chosen based on the model
   #   where there is a single non-null value, based on the intuition that
@@ -31,62 +24,62 @@ mle_point_normal_logscale_grad <- function(x, s, g, control) {
   # }
   
   fn <- function(par) {
-    -loglik_point_normal(x,
-                         s,
-                         w = 1 - (1/(1 + exp(par[1]))),
-                         a = exp(par[2]))
+    -loglik_normal(x,
+                   s,
+                   mu = par[1],
+                   a = exp(par[2]))
   }
   
   gr <- function(par) {
-    grad_negloglik_logscale_point_normal(x,
-                                         s,
-                                         w = 1 - (1/(1 + exp(par[1]))),
-                                         a = exp(par[2]))
+    grad_negloglik_logscale_normal(x,
+                                   s,
+                                   mu = par[1],
+                                   a = exp(par[2]))
   }
   
-  startpar <- c(0, 0) # default
-  if (!is.null(g$pi0)) {
-    startpar[1] <- log(1 / g$pi0 - 1)
+  startpar <- c(mean(x), -log(max(1, var(x) - mean(s^2)))) # default
+  if (!is.null(g$mu)) {
+    startpar[1] <- g$mu
   }
   if (!is.null(g$a)) {
     startpar[2] <- log(g$a)
   }
   
-  uu <- optimize_it(startpar, fn, gr, control,
-                    mle_point_normal_hilo(x, s, fix_pi0 = FALSE))
+  uu <- optimize_it_mu(startpar, fn, gr, control,
+                    mle_point_normal_hilo(x, s, fix_mu = FALSE))
   
-  return(list(pi0 = 1 / (1 + exp(uu$par[1])),
+  return(list(mu = uu$par[1],
               a = exp(uu$par[2]),
               val = uu$value))
 }
 
 
-mle_point_normal_logscale_fixed_pi0 <- function(x, s, g, control) {
+mle_normal_logscale_fixed_mu <- function(x, s, g, control) {
   
   fn <- function(par) {
-    -loglik_point_normal(x, s, 1 - g$pi0, a = exp(par[1]))
+    -loglik_normal(x, s, g$mu, a = exp(par[1]))
   }
   
   gr <- function(par) {
-    grad_negloglik_logscale_point_normal(x, s, 1 - g$pi0, a = exp(par[1]))[2]
+    grad_negloglik_logscale_normal(x, s, g$mu, a = exp(par[1]))[2]
   }
   
   if (!is.null(g$a)) {
     startpar <- log(g$a)
   } else {
-    startpar <- 0 # default
+    startpar <- -log(max(1, var(x) - mean(s^2))) # default
   }
   
   uu <- optimize_it(startpar, fn, gr, control,
-                    mle_point_normal_hilo(x, s, fix_pi0 = TRUE), x, s)
+                    mle_point_normal_hilo(x, s, fix_mu = TRUE), x, s)
   
-  return(list(pi0 = g$pi0, a = exp(uu$par[1]), val = uu$value))
+  return(list(mu = g$mu, a = exp(uu$par[1]), val = uu$value))
 }
 
 
 #' @importFrom stats optim
 #'
-optimize_it <- function(startpar, fn, gr, control, hilo, x, s) {
+optimize_it_mu <- function(startpar, fn, gr, control, hilo, x, s) {
   uu <- try(optim(startpar, fn, gr, method = "BFGS",
                   control = control),
             silent=TRUE)
@@ -96,8 +89,10 @@ optimize_it <- function(startpar, fn, gr, control, hilo, x, s) {
   # complain of infinite values, possibly because of extreme values of
   # the parameters?
   
-  uu <- try(optim(startpar, fn, gr, method = "L-BFGS-B",
-                  lower = hilo$lo, upper = hilo$hi, control = control))
+  #if (class(uu) == "try-error") {
+  #  uu <- try(optim(startpar, fn, gr, method = "L-BFGS-B",
+  #                lower = hilo$lo, upper = hilo$hi, control = control))
+  #}
   
   if (class(uu) == "try-error") {
     saveRDS(list(startpar = startpar, x = x, s = s, control = control),
@@ -113,23 +108,23 @@ optimize_it <- function(startpar, fn, gr, control, hilo, x, s) {
 # Get upper and lower bounds for optim in case the first attempt at
 # optimization fails.
 #
-mle_point_normal_hilo <- function(x, s, fix_pi0) {
-  maxvar <- max(x^2 - s^2)
-  
-  minvar <- (min(s) / 10)^2
-  if (minvar < 1e-8) {
-    minvar <- 1e-8
-  }
-  
-  # Bounds for log(a):
-  lo <- -log(maxvar)
-  hi <- -log(minvar)
-  
-  if (!fix_pi0) {
-    n <- length(x)
-    lo <- c(log(1/n), lo)
-    hi <- c(log(n), hi)
-  }
-  
-  return(list(lo = lo, hi = hi))
-}
+#mle_normal_hilo <- function(x, s, fix_mu) {
+#  maxvar <- max(x^2 - s^2)
+#  
+#  minvar <- (min(s) / 10)^2
+#  if (minvar < 1e-8) {
+#    minvar <- 1e-8
+#  }
+#  
+#  # Bounds for log(a):
+#  lo <- -log(maxvar)
+#  hi <- -log(minvar)
+#  
+#  if (!fix_mu) {
+#    n <- length(x)
+#    lo <- c(min(x) - 6*max(s), lo)
+#    hi <- c(max(x) + 6*max(s), hi)
+#  }
+#  
+#  return(list(lo = lo, hi = hi))
+#}
