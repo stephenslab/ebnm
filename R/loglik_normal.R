1631bd4bf19f2af5a85ce25f1f58463ab227b076
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-11 22:03:53 -0500

    Updated loglik function for normal prior with no point mass, mean mu

--- R/loglik_normal.R
+++ R/loglik_normal.R
@@ -1,31 +1,7 @@
-# Functions to compute the log likelihood under the normal prior.
+# Functions to compute the log likelihood under the normal prior with no point-mass and mean mu.
+# a is the precision, like everywhere else
 
-loglik_point_normal = function(x, s, w, a) {
-  sum(vloglik_point_normal(x, s, w, a))
-}
-
-# Return log((1 - w)f + wg) as a vector (deal with cases w = 1 and w = 0
-#   separately for stability).
-#
 #' @importFrom stats dnorm
-#'
-vloglik_point_normal = function(x, s, w, a) {
-  if (w <= 0) {
-    return(dnorm(x, 0, s, log = TRUE))
-  }
-  
-  lg <- dnorm(x, 0, sqrt(s^2 + 1/a), log = TRUE)
-  if (w >= 1) {
-    return(lg)
-  }
-  
-  lf <- dnorm(x, 0, s, log = TRUE)
-  lfac <- pmax(lg, lf)
-  result <- lfac + log((1 - w) * exp(lf - lfac) + w * exp(lg - lfac))
-  
-  # Deal with zero sds:
-  result[s == 0 & x == 0] <- log(1 - w)
-  result[s == 0 & x != 0] <- log(w) + lg[s == 0 & x != 0]
-  
-  return(result)
+loglik_normal = function(x, s, ,u, a) {
+  sum(dnorm(x, mu, sqrt(s^2 + 1/a), log = TRUE))
 }
