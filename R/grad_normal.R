ada4d014efd5ae3d4bb8fcffa2cc4faa8f2de27f
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-12 17:06:29 -0500

    Fixing errors in the gradient formula

--- R/grad_normal.R
+++ R/grad_normal.R
@@ -4,8 +4,8 @@
 grad_negloglik_logscale_normal = function(x, s, mu, a) {
   vinv = (1 / (s^2 + 1/a))
   
-  grad_mu = -sum((x - mu) / vinv)
-  grad_t = 0.5 * (1/a^2) * sum(((x - mu) / vinv)^2 - vinv)
-  grad = c(grad_mu, grad_t)
+  grad_mu = -sum((x - mu) * vinv)
+  grad_a = 0.5 * (1/a) * sum(((x - mu) * vinv)^2 - vinv)
+  grad = c(grad_mu, grad_a)
   return(grad)
 }
