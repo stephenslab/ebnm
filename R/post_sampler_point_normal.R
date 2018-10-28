9b5a5c2ae5d96c79ac1bd6242498875efe7d93c9
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-28 17:49:40 -0500

    Adding parameter mu

--- R/post_sampler_point_normal.R
+++ R/post_sampler_point_normal.R
@@ -6,6 +6,8 @@
 #
 # @param a The variance of the non-null component in the fitted prior.
 #
+# @param mu The mean of the distributions
+#
 # @param nsamp The number of samples to return per observation.
 #
 # @return An nsamp by length(x) matrix containing samples from the
@@ -13,13 +15,13 @@
 #
 #' @importFrom stats rbinom rnorm
 #'
-post_sampler_point_normal <- function(x, s, w, a, nsamp) {
-  wpost <- wpost_normal(x, s, w, a)
-  pmean_cond <- pmean_cond_normal(x, s, a)
+post_sampler_point_normal <- function(x, s, w, a, mu, nsamp) {
+  wpost <- wpost_normal(x, s, w, a, mu)
+  pmean_cond <- pmean_cond_normal(x, s, a, mu)
   pvar_cond <- pvar_cond_normal(s, a)
 
   is_nonnull <- rbinom(nsamp*length(wpost), 1, rep(wpost, nsamp))
   samp <- is_nonnull * rnorm(nsamp*length(wpost), rep(pmean_cond, nsamp),
-                             rep(sqrt(pvar_cond), nsamp))
+                             rep(sqrt(pvar_cond), nsamp)) + (1 - is_nonnull) * mu
   return(matrix(samp, nrow=nsamp, byrow=T))
 }
