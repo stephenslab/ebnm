faa3783fbf356a112343f5b064ba1a0fe28f10c6
Andrew Goldstein <andrewg3311@gmail.com>
2018-10-28 14:11:34 -0500

    Removing (no longer needed after point_normal update)

--- R/loglik_normal.R
+++
@@ -1,7 +0,0 @@
-# Functions to compute the log likelihood under the normal prior with no point-mass and mean mu.
-# a is the precision, like everywhere else
-
-#' @importFrom stats dnorm
-loglik_normal = function(x, s, mu, a) {
-  sum(dnorm(x, mu, sqrt(s^2 + 1/a), log = TRUE))
-}
