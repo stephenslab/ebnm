d386caa354a17a025b12e4effcf6d6cd4db76459
Jason Willwerscheid <willwerscheid@uchicago.edu>
2019-03-06 14:35:40 -0500

    Fix ebnm interface tests.

--- tests/testthat/test_ebnm_interface.R
+++ tests/testthat/test_ebnm_interface.R
@@ -5,25 +5,27 @@ test_that("'ebnm' calls give same results as 'ebnm_point_normal' or 'ebnm_point_
   set.seed(1)
   s = rgamma(n,1,1)
   x = rnorm(n,0,1+s)
-  
+
   ### test point_normal
   # fix nothing
-  ebnm.res1 = ebnm(x, s, "point_normal", g = NULL, fix_mu = F)
-  ebnm.pn.res1 = ebnm_point_normal(x, s)
+  ebnm.res1 = ebnm(x, s, "point_normal", g = NULL, fix_mu = FALSE)
+  ebnm.pn.res1 = ebnm_point_normal(x, s, g = NULL, fix_mu = FALSE)
   expect_equal(ebnm.res1, ebnm.pn.res1)
   # fix pi0
-  ebnm.res2 = ebnm(x, s, "point_normal", g = list(pi0 = .2), fix_pi0 = T, fix_mu = F)
-  ebnm.pn.res2 = ebnm_point_normal(x, s, g = list(pi0 = .2), fix_pi0 = T)
+  ebnm.res2 = ebnm(x, s, "point_normal", g = list(pi0 = .2), fix_pi0 = TRUE,
+                   fix_mu = FALSE)
+  ebnm.pn.res2 = ebnm_point_normal(x, s, g = list(pi0 = .2), fix_pi0 = TRUE,
+                                   fix_mu = FALSE)
   expect_equal(ebnm.res2, ebnm.pn.res2)
   # fix mu
   ebnm.res3 = ebnm(x, s, "point_normal")
-  ebnm.pn.res3 = ebnm_point_normal(x, s, g = list(mu = 0), fix_mu = T)
+  ebnm.pn.res3 = ebnm_point_normal(x, s)
   expect_equal(ebnm.res3, ebnm.pn.res3)
   # fix both
-  ebnm.res4 = ebnm(x, s, "point_normal", g = list(pi0 = .2, mu = 0), fix_pi0 = T, fix_mu = T)
-  ebnm.pn.res4 = ebnm_point_normal(x, s, g = list(pi0 = .2, mu = 0), fix_pi0 = T, fix_mu = T)
+  ebnm.res4 = ebnm(x, s, "point_normal", g = list(pi0 = .2, mu = 0), fix_pi0 = TRUE, fix_mu = TRUE)
+  ebnm.pn.res4 = ebnm_point_normal(x, s, g = list(pi0 = .2, mu = 0), fix_pi0 = TRUE, fix_mu = TRUE)
   expect_equal(ebnm.res4, ebnm.pn.res4)
-  
+
   ### test point_laplace
   ebnm.res5 = ebnm(x, s, "point_laplace")
   ebnm.pl.res5 = ebnm_point_laplace(x, s)
