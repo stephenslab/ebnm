a5862e156686c2e0c0bee9eead67598785daadfc
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-02-26 21:30:34 -0600

    Added third plot to shrink_intro_plots.R.

--- inst/code/shrink_intro_plots.R
+++ inst/code/shrink_intro_plots.R
@@ -7,10 +7,10 @@ library(cowplot)
 render("shrink_intro.Rmd")
 
 # Compile the results needed to generate the plots.
-pdat <- data.frame(u     = u,
-                   mle   = x,
-                   est.n = coef(fit_normal)
-)
+pdat <- data.frame(u      = u,
+                   mle    = x,
+                   est.n  = coef(fit_normal),
+                   est.pn = coef(fit_pn))
 
 # Plot true mean vs. MLE.
 lims <- c(-0.55,5.05)
@@ -41,4 +41,21 @@ p2 <- ggplot(pdat,aes(x = u,y = est.n)) +
   theme(plot.title = element_text(face = "plain",size = 12),
         plot.subtitle = element_text(face = "plain",size = 12))
 
-print(plot_grid(p1,p2,nrow = 1,ncol = 2))
+# Plot true mean vs. posterior estimate from fit_pn.
+p3 <- ggplot(pdat,aes(x = u,y = est.pn)) +
+  geom_point() +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",linetype = "dotted") +
+  labs(x = "true value ",
+       y = "estimate",
+       title = "EB with point-normal prior",
+       subtitle = sprintf("MSE = %0.3f",mean(err_shrink_pn))) +
+  xlim(lims) +
+  ylim(lims) +
+  theme_cowplot(font_size = 12) +
+  theme(plot.title = element_text(face = "plain",size = 12),
+        plot.subtitle = element_text(face = "plain",size = 12))
+
+print(plot_grid(p1,p2,p3,nrow = 1,ncol = 3))
+ggsave("shrink_intro.pdf",
+       plot_grid(p1,p2,p3,nrow = 1,ncol = 3),
+       height = 2.75,width = 7)
